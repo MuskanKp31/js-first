723cc143a48834615c5cc560d82322c83967021e
MuskanKp31 <muskan.dataanalysts@gmail.com>
2024-11-07 08:39:58 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,30 @@
+/*
+refer ecma standards for more specification  --- > https:/tc39.es.......
+*/
+
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3);
+
+let name ="muskan"
+let agr = 20
+let isLoggedIn = false
+let state;
+
+// number =>2 to poer 53
+// string => "" / '' 
+// bigInt 
+// boolean => true/false
+// null => standalone value it is representation of empty value
+// undefined => when value is not defined
+// symbol => unique
+
+
+// object
+
+// null is object
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
