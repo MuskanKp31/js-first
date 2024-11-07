03913c1be9c057caf525c6bce8f2dbdb53d56742
MuskanKp31 <muskan.dataanalysts@gmail.com>
2024-11-07 08:39:36 +0000

    edited test file added notes

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,10 @@
+/* 
+notes inside code :::::::::::
+
+create a folder (first-js)
+open vs code -> create a file (test.js) -> write console.log("name");
+for print write command on terminal -> node file path (node first-js/test.js) it will print name
+
+*/
+
 console.log("Muskan")
